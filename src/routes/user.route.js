9e9c03d4ad988b844aceb175f8be5e5590b00504
Paulo Victor <paulovictorlima418@gmail.com>
2021-10-09 22:02:44 -0300

    Create route to add new user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
+const database = require('../database/index');
+
 const route = express.Router();
 
 route.get('/user', (request, response) => {
-  return response.status(201).send('ok');
+  return response.status(201).json(database);
+});
+
+route.post('/user', (request, response) => {
+  const { name, cpf, email, date } = request.body;
+
+  database.push({
+    name,
+    cpf,
+    email,
+    date,
+    created_at: new Date(),
+    updated_at: new Date()
+  });
+
+  return response.status(201).json({message: 'User created!'})
 });
 
 module.exports = route;
